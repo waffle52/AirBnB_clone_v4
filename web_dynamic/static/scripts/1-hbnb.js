910066f4b39b9909f6f2a3c9a7e28f7e1e1ca20f
Beardocracy <travisjbearden@gmail.com>
2020-05-16 20:50:03 +0000

    Added functionality of adding or subtracting to the list

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  const amenityList = [];
+  $('INPUT:checkbox').change(function () {
+    if ($(this).prop('checked') === true) {
+      amenityList.push($(this).attr('data-name'));
+    } else {
+      const i = amenityList.indexOf($(this).attr('data-name'));
+      if (i != -1) {
+        amenityList.splice(i, 1);
+      }
+    }
+    $('h4').text(amenityList.join(', '));
+  });
+});
